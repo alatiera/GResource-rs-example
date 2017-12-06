c794014cd44f889fb35ec0d7386e2afe3af5a410
Jordan Petridis <jordanpetridis@protonmail.com>
2017-12-06 10:30:16 +0200

    Gtk+ Hello World.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate gtk;
+
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    gtk::init().expect("Failed to initialize gtk+");
+
+    let builder = gtk::Builder::new_from_string(include_str!("../resources/window.ui"));
+    let window: gtk::Window = builder.get_object("window").unwrap();
+    window.show_all();
+
+    window.connect_delete_event(|_, _| {
+        // Stop the main loop.
+        gtk::main_quit();
+        // Let the default handler destroy the window.
+        Inhibit(false)
+    });
+
+    gtk::main();
 }
